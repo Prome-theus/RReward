bf4e6e4ab60261726aa195aeacf45b7c219da0a5
Shivamxagrawal <agrawalshivam57@gmail.com>
2024-03-16 19:21:12 +0530

    market

--- src/app/MarketPlace/page.js
+++ src/app/MarketPlace/page.js
@@ -1,9 +1,67 @@
-import React from 'react'
+"use client"
+import { useState } from 'react';
+import Marketplace from '@/src/components/MarketPlace';
 
-const page = () => {
+const allProducts = [
+  { id: 1, name: 'The Catalyzer', price: 16.00, image: 'https://dummyimage.com/420x260' },
+  { id: 2, name: 'Shooting Stars', price: 21.15, image: 'https://dummyimage.com/421x261' },
+  { id: 3, name: 'Neptune', price: 12.00, image: 'https://dummyimage.com/422x262' },
+  { id: 1, name: 'The Catalyzer', price: 16.00, image: 'https://dummyimage.com/420x260' },
+  { id: 2, name: 'Shooting Stars', price: 21.15, image: 'https://dummyimage.com/421x261' },
+  { id: 3, name: 'Neptune', price: 12.00, image: 'https://dummyimage.com/422x262' },
+  { id: 1, name: 'The Catalyzer', price: 16.00, image: 'https://dummyimage.com/420x260' },
+  { id: 2, name: 'Shooting Stars', price: 21.15, image: 'https://dummyimage.com/421x261' },
+  { id: 3, name: 'Neptune', price: 12.00, image: 'https://dummyimage.com/422x262' },
+  { id: 1, name: 'The Catalyzer', price: 16.00, image: 'https://dummyimage.com/420x260' },
+  { id: 2, name: 'Shooting Stars', price: 21.15, image: 'https://dummyimage.com/421x261' },
+  { id: 3, name: 'Neptune', price: 12.00, image: 'https://dummyimage.com/422x262' },
+  { id: 1, name: 'The Catalyzer', price: 16.00, image: 'https://dummyimage.com/420x260' },
+  { id: 2, name: 'Shooting Stars', price: 21.15, image: 'https://dummyimage.com/421x261' },
+  { id: 3, name: 'Neptune', price: 12.00, image: 'https://dummyimage.com/422x262' },
+  { id: 1, name: 'The Catalyzer', price: 16.00, image: 'https://dummyimage.com/420x260' },
+  { id: 2, name: 'Shooting Stars', price: 21.15, image: 'https://dummyimage.com/421x261' },
+  { id: 3, name: 'Neptune', price: 12.00, image: 'https://dummyimage.com/422x262' },
+  { id: 1, name: 'The Catalyzer', price: 16.00, image: 'https://dummyimage.com/420x260' },
+  { id: 2, name: 'Shooting Stars', price: 21.15, image: 'https://dummyimage.com/421x261' },
+  { id: 3, name: 'Neptune', price: 12.00, image: 'https://dummyimage.com/422x262' },
+];
+
+const itemsPerPage = 12;
+
+const HomePage = () => {
+  const [products, setProducts] = useState(allProducts);
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const indexOfLastItem = currentPage * itemsPerPage;
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+  const currentProducts = products.slice(indexOfFirstItem, indexOfLastItem);
+
+  const nextPage = () => {
+    setCurrentPage(currentPage + 1);
+  };
+
+  const prevPage = () => {
+    setCurrentPage(currentPage - 1);
+  };
+
+  const isLastPage = indexOfLastItem >= products.length;
+
+  const sortByPrice = () => {
+    const sortedProducts = [...products].sort((a, b) => a.price - b.price);
+    setProducts(sortedProducts);
+  };
+
   return (
-    <div>MarketPlace</div>
-  )
-}
+    <div>
+      <h1 className="text-3xl font-bold mb-4">Marketplace</h1>
+      <button onClick={sortByPrice} className="p-2 border rounded-md mr-2">Sort by Points</button>
+      <Marketplace products={currentProducts} />
+      <div className="flex justify-center mt-4">
+        <button onClick={prevPage} disabled={currentPage === 1} className="p-2 mr-2 bg-gray-300 text-gray-600 rounded-md">Prev</button>
+        <button onClick={nextPage} disabled={isLastPage} className="p-2 bg-blue-500 text-white rounded-md">Next</button>
+      </div>
+    </div>
+  );
+};
 
-export default page
+export default HomePage;
