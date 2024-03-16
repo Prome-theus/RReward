c6f0dccbd56e68e0a2a61e79029b453b8c5cf373
verma-varun1 <vermavarunvv10@gmail.com>
2024-03-16 23:17:30 +0530

    review

--- src/app/Admin/page.js
+++ src/app/Admin/page.js
@@ -1,9 +1,9 @@
-import React from "react";
+import React from 'react'
 
-const PostReview = () => {
+const page = () => {
   return (
     <div>
-      <h2 className=" md:col-span-5 font-bold text-3xl text-center text-gray-900">Product Review</h2>
+      <h2 className=" md:col-span-5 font-bold text-3xl text-center text-gray-900">Adding Product</h2>
       
       <div class="min-h-screen bg-gray-900 flex items-center justify-center">
   
@@ -20,29 +20,37 @@ const PostReview = () => {
 
 
     <div class="md:col-span-5">
-      <label class="block text-gray-700 text-sm font-bold mb-2 text-center" htmlFor="Purchase Date">
-        Purchase Date
+      <label class="block text-gray-700 text-sm font-bold mb-2 text-center" htmlFor="Product Category">
+        Product Category
       </label>
-      <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="purdate" type="date"/>
+      <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="category" name=' Your Category' type='text' placeholder='Category'/>
     </div>
 
     <div class="md:col-span-5">
-      <label class="block text-gray-700 text-sm font-bold mb-2 text-center" htmlFor="Product Review">
-        Product Review
+      <label class="block text-gray-700 text-sm font-bold mb-2 text-center" htmlFor="Description">
+        Product Description
       </label>
-      <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="proreview" type="textarea" placeholder="Product Review"/>
+      <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="proreview" type="textarea" placeholder="Product Desciption"/>
     </div>
 
+    <div class="md:col-span-5">
+      <label class="block text-gray-700 text-sm font-bold mb-2 text-center" htmlFor="Product Pricing">
+        Product Price
+      </label>
+      <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="proreview" type="number" placeholder="Product Price"/>
+    </div>
+    
     <div class="md:col-span-5">
       <label class="block text-gray-700 text-sm font-bold mb-2 text-center" htmlFor="Product Image">
         Product Image
       </label>
       <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="proreview" type="file" placeholder="Choose Image"/>
     </div>
+
     
     <div class="md:col-span-5 text-center">
       <button class="bg-blue-500 text-white rounded-md p-2 text-sm text-center m-2" type="button">
-        Post Review
+        Upload
       </button>
     </div>
     
@@ -50,7 +58,7 @@ const PostReview = () => {
   </form>
 </div>
     </div>
-  );
-};
+  )
+}
 
-export default PostReview;
+export default page
