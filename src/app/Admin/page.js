cf4dc4bc221f5c45e3fcb886427830cac2986f4a
verma-varun1 <vermavarunvv10@gmail.com>
2024-03-17 01:54:40 +0530

    varun

--- src/app/Admin/page.js
+++ src/app/Admin/page.js
@@ -3,50 +3,52 @@ import React from 'react'
 const page = () => {
   return (
     <div>
-      <h2 className=" md:col-span-5 font-bold text-3xl text-center text-gray-900">Adding Product</h2>
+      <h2 className=" font-bold text-3xl bg-gray-900 text-center pt-10 pb-5 text-white">Adding Offer</h2>
       
-      <div className="min-h-screen bg-gray-900 flex items-center justify-center">
+      <div className="bg-gray-900 flex items-center justify-center">
   
   
-  <form className="bg-white shadow-md rounded px-8 pt-8 pb-8 mb-6">
+  <form className="bg-white shadow-md rounded px-8 pt-5 pb-8 mb-6 w-11/12">
     
     
     <div className="md:col-span-5">
-      <label className="block text-gray-700 text-sm font-bold mb-2 text-center" htmlFor="Poduct Name">
-       Product Name
+      <label className="block text-gray-700 text-sm font-bold mb-2 text-center" htmlFor="Offer Name">
+       Offer Name
       </label>
-      <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="proname" type="text" placeholder="Product Name" />
+      <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="proname" type="text" placeholder="Offer Name" />
     </div>
-
-
-    <div className="md:col-span-5">
-      <label className="block text-gray-700 text-sm font-bold mb-2 text-center" htmlFor="Product Category">
-        Product Category
-      </label>
-      <label className="block text-gray-700 text-sm mb-2 text-left"><input type='radio' name="category" value="Smartphones" ></input>Smartphones</label>  
-      <label className="block text-gray-700 text-sm mb-2 text-left"><input type='radio' name="category" value="Laptops" ></input>Laptops</label>
-      <label className="block text-gray-700 text-sm mb-2 text-left"><input type='radio' name="category" value="Smartwatches" ></input>Smartwatches</label>
-      <label className="block text-gray-700 text-sm mb-2 text-left"><input type='radio' name="category" value="Home Appliances" ></input>Home Appliances</label>
-      <label className="block text-gray-700 text-sm mb-2 text-left"><input type='radio' name="category" value="Computer Pheriperals" ></input>Computer Pheriperals</label>
-      <label className="block text-gray-700 text-sm mb-2 text-left"><input type='radio' name="category" value="Headphones" ></input>Headphones</label>
-    </div>
+  
    <div className="md:col-span-5">
       <label className="block text-gray-700 text-sm font-bold mb-2 text-center" htmlFor="Description">
-        Product Description
+        Offer Description
       </label>
-      <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="proreview" type="textarea" placeholder="Product Desciption"/>
+      <textarea className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="proreview" type="textarea" placeholder="Offer Desciption"/>
     </div>
 
     <div className="md:col-span-5">
-      <label className="block text-gray-700 text-sm font-bold mb-2 text-center" htmlFor="Product Pricing">
-        Product Price
+      <label className="block text-gray-700 text-sm font-bold mb-2 text-center" htmlFor="Offer Pricing">
+        Offer Price
       </label>
-      <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="proreview" type="number" placeholder="Product Price"/>
+      <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="proreview" type="number" placeholder="Offer Price"/>
     </div>
+
+    <div className="md:col-span-5">
+            <label
+              className="block text-gray-700 text-sm font-bold mb-2 text-center"
+              htmlFor="Offer End Date"
+            >
+              Offer End Date
+            </label>
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              id="purdate"
+              type="date"
+            />
+          </div>
     
     <div className="md:col-span-5">
-      <label className="block text-gray-700 text-sm font-bold mb-2 text-center" htmlFor="Product Image">
-        Product Image
+      <label className="block text-gray-700 text-sm font-bold mb-2 text-center" htmlFor="Offer Image">
+        Offer Image
       </label>
 
       <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="proreview" type="file" placeholder="Choose Image"/>
