648c084498f17cc028dc97b9254d53c0bd2bed9a
Shivamxagrawal <agrawalshivam57@gmail.com>
2024-03-18 13:48:58 +0530

    landing

--- src/app/page.js
+++ src/app/page.js
@@ -72,28 +72,62 @@ const Page = () => {
         <div className="gap-16 items-center py-8 px-4 mx-auto max-w-screen-xl lg:grid lg:grid-cols-2 lg:py-16 lg:px-6">
           <div className="font-light text-gray-500 sm:text-lg dark:text-gray-400">
             <h2 className="mb-4 text-4xl font-extrabold text-gray-900 dark:text-white">
-              We didn't reinvent the wheel
+              REWARD FOR REVIEW
             </h2>
             <p className="mb-4">
-              We are strategists, designers and developers. Innovators and
-              problem solvers. Small enough need.
+            Compose a detailed review outlining your experience with the product, covering its performance, durability, ease of use, and any other pertinent factors. Offer genuine insights to aid potential buyers in making informed decisions.
             </p>
-            <p>We are strategists, design.</p>
+            
           </div>
           <div className="grid grid-cols-2 gap-4 mt-8">
             <img
               className="w-full rounded-lg"
-              src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/content/office-long-2.png"
+              src="reward image.png"
               alt="office content 1"
             />
             <img
               className="mt-4 w-full lg:mt-10 rounded-lg"
-              src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/content/office-long-1.png"
+              src="review.jpg"
               alt="office content 2"
             />
           </div>
         </div>
       </section>
+      <section className=" dark:bg-gray-800">
+        <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
+          <div className="max-w-screen-lg text-gray-500 sm:text-lg dark:text-gray-400">
+            <h2 className="mb-4 text-4xl font-bold text-gray-900 dark:text-white">
+              Helping users for better shopping and product experience
+              <span className="font-extrabold"></span> 
+              
+            </h2>
+            <p className="mb-4 font-light">
+              Exciting rewards for every review you post.
+              Reward Review makes the service provider understand the customers and the reponse of the users on the products.
+            </p>
+            <p className="mb-4 font-medium"
+            ></p>
+            <a
+              href="/AboutUs"
+              className="inline-flex items-center font-medium text-blue-600 hover:text-blue-800 dark:text-blue-500 dark:hover:text-blue-700"
+            >
+              Learn more
+              <svg
+                className="ml-1 w-6 h-6"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+                  clipRule="evenodd"
+                ></path>
+              </svg>
+            </a>
+          </div>
+        </div>
+      </section> 
       </div>
     </main>
   );
