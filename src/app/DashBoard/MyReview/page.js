c731820c225cb8d5be4efe2a7ed1641573854b4a
verma-varun1 <vermavarunvv10@gmail.com>
2024-04-10 22:55:24 +0530

    varun

--- src/app/DashBoard/MyReview/page.js
+++ src/app/DashBoard/MyReview/page.js
@@ -4,17 +4,15 @@ import cardList from "./data";
 export default function review() {
   return (
     <div>
-      <head>
         <title>My Review</title>
-      </head>
 
-      <h1 className="text-center font-bold text-6xl py-10 text-amber-600 font-mono">My Reviews</h1>
+      <p className="text-center font-bold text-6xl py-10 text-amber-600 font-mono">My Reviews</p>
 
       <main className="container mx-auto py-10 px-8 ">
         <div className="grid lg:grid-cols-3 gap-10 md:grid-cols-2 grid-col-1">
           
-          {cardList.map(card => (
-            <div className="shadow-lg rounded-lg">
+          {cardList.map((card , n) => (
+            <div key={n} className="shadow-lg rounded-lg">
             
                 <img className="rounded-t-lg" src={card.img} alt=""/>
                 <div className="p-5">
@@ -29,3 +27,4 @@ export default function review() {
     </div>
   );
 }
+
